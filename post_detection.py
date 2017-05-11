343ef49bb94dbc0709d781a5330da440f008e8cc
KK <k.o.karnatak@gmail.com>
2017-05-11 19:07:02 +0200

    Pushing changes to github first time

---
+++ post_detection.py
@@ -0,0 +1,16 @@
+import sys
+
+from PyQt4 import QtGui
+
+
+def show_image(image_path='detected.jpg'):
+    app = QtGui.QApplication(sys.argv)
+    pixmap = QtGui.QPixmap(image_path)
+    screen = QtGui.QLabel()
+    screen.setPixmap(pixmap)
+    screen.showFullScreen()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    show_image()
